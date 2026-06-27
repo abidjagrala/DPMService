7099f19a0d4de3c7445dc80f01f4765ba3bec891
abidjagrala <34119137+abidjagrala@users.noreply.github.com>
2026-06-27 18:21:20 +0530

    Add dpm.dnscloud.in to default ALLOWED_HOSTS

--- dpmservice/settings.py
+++ dpmservice/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-6wm*4r=(njrnm^m%jg+rv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default='True', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,testserver', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,testserver,dpm.dnscloud.in', cast=Csv())
 
 
 # Application definition
