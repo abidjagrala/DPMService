a2032a9b2b818a1980c16e626c58e92b43a6d544
abidjagrala <34119137+abidjagrala@users.noreply.github.com>
2026-06-28 12:38:27 +0530

    Add defensive error handling to all dashboard views with logging

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
@@ -11,21 +12,50 @@ from masters.models import City, State
 
 from . import services
 
+logger = logging.getLogger(__name__)
+
+
+def _safe(fn, default, *args, **kwargs):
+    try:
+        return fn(*args, **kwargs)
+    except Exception:
+        logger.exception('Dashboard service error in %s', fn.__name__)
+        return default
+
 
 @login_required
 @require_http_methods(['GET'])
 def dashboard_view(request):
     user = request.user
-    ctx = services.get_all_kpis(user)
-    ctx['recent_tickets'] = services.get_recent_tickets(user)
-    ctx['recent_activities'] = services.get_recent_activities(user)
-    ctx['expiry_alerts'] = services.get_expiry_alerts(user)
-    ctx['warranty_alerts'] = services.get_asset_warranty_alerts(user)
-    ctx['client_summary'] = services.get_client_summary(user)
-    ctx['homeworker_summary'] = services.get_homeworker_summary(user)
-    ctx['domain_hosting_panel'] = services.get_domain_hosting_panel(user)
-    ctx['my_tasks'] = services.get_my_tasks(user)
-    ctx['recent_comments'] = services.get_recent_comments(user)
+    ctx = _safe(services.get_all_kpis, {
+        'total_clients': 0, 'total_employees': 0, 'total_homeworkers': 0,
+        'total_tickets': 0, 'open_tickets': 0, 'in_progress_tickets': 0,
+        'completed_tickets': 0, 'tickets_today': 0,
+        'total_assets': 0, 'assigned_assets': 0, 'available_assets': 0, 'maintenance_assets': 0,
+        'total_devices': 0, 'active_devices': 0, 'offline_devices': 0, 'repair_devices': 0,
+        'total_domains': 0, 'active_domains': 0, 'expiring_domains_30': 0,
+        'total_hosting': 0, 'active_hosting': 0, 'expiring_hosting_30': 0,
+    }, user)
+    ctx['recent_tickets'] = _safe(services.get_recent_tickets, [], user)
+    ctx['recent_activities'] = _safe(services.get_recent_activities, [], user)
+    ctx['expiry_alerts'] = _safe(services.get_expiry_alerts, {
+        'domains_30': [], 'domains_15': [], 'domains_7': [],
+        'hosting_30': [], 'hosting_15': [], 'hosting_7': [],
+    }, user)
+    ctx['warranty_alerts'] = _safe(services.get_asset_warranty_alerts, [], user)
+    ctx['client_summary'] = _safe(services.get_client_summary, Client.objects.none(), user)
+    ctx['homeworker_summary'] = _safe(services.get_homeworker_summary, {
+        'total': 0, 'assigned_assets': 0, 'assigned_devices': 0,
+        'open_tickets': 0, 'closed_tickets': 0,
+    }, user)
+    ctx['domain_hosting_panel'] = _safe(services.get_domain_hosting_panel, {
+        'domains': [], 'hosting': [],
+    }, user)
+    ctx['my_tasks'] = _safe(services.get_my_tasks, {
+        'assigned_tickets': [], 'pending_count': 0,
+        'due_today_count': 0, 'overdue_count': 0,
+    }, user)
+    ctx['recent_comments'] = _safe(services.get_recent_comments, [], user)
 
     # Filter options
     if user.is_client:
@@ -46,22 +76,32 @@ def dashboard_view(request):
 @login_required
 @require_http_methods(['GET'])
 def dashboard_kpis(request):
-    return render(request, 'dashboard/_kpi_cards.html', services.get_all_kpis(request.user))
+    empty = {
+        'total_clients': 0, 'total_employees': 0, 'total_homeworkers': 0,
+        'total_tickets': 0, 'open_tickets': 0, 'in_progress_tickets': 0,
+        'completed_tickets': 0, 'tickets_today': 0,
+        'total_assets': 0, 'assigned_assets': 0, 'available_assets': 0, 'maintenance_assets': 0,
+        'total_devices': 0, 'active_devices': 0, 'offline_devices': 0, 'repair_devices': 0,
+        'total_domains': 0, 'active_domains': 0, 'expiring_domains_30': 0,
+        'total_hosting': 0, 'active_hosting': 0, 'expiring_hosting_30': 0,
+    }
+    return render(request, 'dashboard/_kpi_cards.html', _safe(services.get_all_kpis, empty, request.user))
 
 
 @login_required
 @require_http_methods(['GET'])
 def dashboard_charts(request):
     user = request.user
+    empty = {'labels': [], 'values': []}
     ctx = {
-        'monthly_trend': services.get_monthly_ticket_trend(user),
-        'tickets_by_status': services.get_tickets_by_status(user),
-        'client_wise_tickets': services.get_client_wise_tickets(user),
-        'staff_productivity': services.get_staff_productivity(user),
-        'asset_status': services.get_asset_status_distribution(user),
-        'device_categories': services.get_device_category_distribution(user),
-        'domain_hosting_overview': services.get_domain_hosting_overview(user),
-        'client_state_dist': services.get_client_state_distribution(user),
+        'monthly_trend': _safe(services.get_monthly_ticket_trend, {'labels': [], 'open': [], 'completed': [], 'total': []}, user),
+        'tickets_by_status': _safe(services.get_tickets_by_status, empty, user),
+        'client_wise_tickets': _safe(services.get_client_wise_tickets, empty, user),
+        'staff_productivity': _safe(services.get_staff_productivity, {'labels': [], 'assigned': [], 'completed': []}, user),
+        'asset_status': _safe(services.get_asset_status_distribution, empty, user),
+        'device_categories': _safe(services.get_device_category_distribution, empty, user),
+        'domain_hosting_overview': _safe(services.get_domain_hosting_overview, empty, user),
+        'client_state_dist': _safe(services.get_client_state_distribution, empty, user),
     }
     return render(request, 'dashboard/_charts.html', ctx)
 
@@ -70,7 +110,7 @@ def dashboard_charts(request):
 @require_http_methods(['GET'])
 def dashboard_recent_tickets(request):
     return render(request, 'dashboard/_recent_tickets.html', {
-        'recent_tickets': services.get_recent_tickets(request.user),
+        'recent_tickets': _safe(services.get_recent_tickets, [], request.user),
     })
 
 
@@ -78,7 +118,7 @@ def dashboard_recent_tickets(request):
 @require_http_methods(['GET'])
 def dashboard_activities(request):
     return render(request, 'dashboard/_activities.html', {
-        'recent_activities': services.get_recent_activities(request.user),
+        'recent_activities': _safe(services.get_recent_activities, [], request.user),
     })
 
 
@@ -86,8 +126,11 @@ def dashboard_activities(request):
 @require_http_methods(['GET'])
 def dashboard_expiry_alerts(request):
     return render(request, 'dashboard/_expiry_alerts.html', {
-        'expiry_alerts': services.get_expiry_alerts(request.user),
-        'warranty_alerts': services.get_asset_warranty_alerts(request.user),
+        'expiry_alerts': _safe(services.get_expiry_alerts, {
+            'domains_30': [], 'domains_15': [], 'domains_7': [],
+            'hosting_30': [], 'hosting_15': [], 'hosting_7': [],
+        }, request.user),
+        'warranty_alerts': _safe(services.get_asset_warranty_alerts, [], request.user),
     })
 
 
@@ -95,7 +138,7 @@ def dashboard_expiry_alerts(request):
 @require_http_methods(['GET'])
 def dashboard_client_summary(request):
     return render(request, 'dashboard/_client_summary.html', {
-        'client_summary': services.get_client_summary(request.user),
+        'client_summary': _safe(services.get_client_summary, Client.objects.none(), request.user),
     })
 
 
@@ -103,7 +146,10 @@ def dashboard_client_summary(request):
 @require_http_methods(['GET'])
 def dashboard_homeworker_summary(request):
     return render(request, 'dashboard/_homeworker_summary.html', {
-        'homeworker_summary': services.get_homeworker_summary(request.user),
+        'homeworker_summary': _safe(services.get_homeworker_summary, {
+            'total': 0, 'assigned_assets': 0, 'assigned_devices': 0,
+            'open_tickets': 0, 'closed_tickets': 0,
+        }, request.user),
     })
 
 
@@ -111,7 +157,9 @@ def dashboard_homeworker_summary(request):
 @require_http_methods(['GET'])
 def dashboard_domain_hosting(request):
     return render(request, 'dashboard/_domain_hosting_panel.html', {
-        'domain_hosting_panel': services.get_domain_hosting_panel(request.user),
+        'domain_hosting_panel': _safe(services.get_domain_hosting_panel, {
+            'domains': [], 'hosting': [],
+        }, request.user),
     })
 
 
@@ -119,7 +167,10 @@ def dashboard_domain_hosting(request):
 @require_http_methods(['GET'])
 def dashboard_my_tasks(request):
     return render(request, 'dashboard/_my_tasks.html', {
-        'my_tasks': services.get_my_tasks(request.user),
+        'my_tasks': _safe(services.get_my_tasks, {
+            'assigned_tickets': [], 'pending_count': 0,
+            'due_today_count': 0, 'overdue_count': 0,
+        }, request.user),
     })
 
 
@@ -127,7 +178,7 @@ def dashboard_my_tasks(request):
 @require_http_methods(['GET'])
 def dashboard_comments(request):
     return render(request, 'dashboard/_recent_comments.html', {
-        'recent_comments': services.get_recent_comments(request.user),
+        'recent_comments': _safe(services.get_recent_comments, [], request.user),
     })
 
 
@@ -144,46 +195,54 @@ def dashboard_quick_actions(request):
 @login_required
 @require_http_methods(['GET'])
 def chart_monthly_trend(request):
-    return JsonResponse(services.get_monthly_ticket_trend(request.user))
+    empty = {'labels': [], 'open': [], 'completed': [], 'total': []}
+    return JsonResponse(_safe(services.get_monthly_ticket_trend, empty, request.user))
 
 
 @login_required
 @require_http_methods(['GET'])
 def chart_tickets_by_status(request):
-    return JsonResponse(services.get_tickets_by_status(request.user))
+    empty = {'labels': [], 'values': []}
+    return JsonResponse(_safe(services.get_tickets_by_status, empty, request.user))
 
 
 @login_required
 @require_http_methods(['GET'])
 def chart_client_wise_tickets(request):
-    return JsonResponse(services.get_client_wise_tickets(request.user))
+    empty = {'labels': [], 'values': []}
+    return JsonResponse(_safe(services.get_client_wise_tickets, empty, request.user))
 
 
 @login_required
 @require_http_methods(['GET'])
 def chart_staff_productivity(request):
-    return JsonResponse(services.get_staff_productivity(request.user))
+    empty = {'labels': [], 'assigned': [], 'completed': []}
+    return JsonResponse(_safe(services.get_staff_productivity, empty, request.user))
 
 
 @login_required
 @require_http_methods(['GET'])
 def chart_asset_status(request):
-    return JsonResponse(services.get_asset_status_distribution(request.user))
+    empty = {'labels': [], 'values': []}
+    return JsonResponse(_safe(services.get_asset_status_distribution, empty, request.user))
 
 
 @login_required
 @require_http_methods(['GET'])
 def chart_device_categories(request):
-    return JsonResponse(services.get_device_category_distribution(request.user))
+    empty = {'labels': [], 'values': []}
+    return JsonResponse(_safe(services.get_device_category_distribution, empty, request.user))
 
 
 @login_required
 @require_http_methods(['GET'])
 def chart_domain_hosting(request):
-    return JsonResponse(services.get_domain_hosting_overview(request.user))
+    empty = {'labels': [], 'values': []}
+    return JsonResponse(_safe(services.get_domain_hosting_overview, empty, request.user))
 
 
 @login_required
 @require_http_methods(['GET'])
 def chart_client_state(request):
-    return JsonResponse(services.get_client_state_distribution(request.user))
+    empty = {'labels': [], 'values': []}
+    return JsonResponse(_safe(services.get_client_state_distribution, empty, request.user))
